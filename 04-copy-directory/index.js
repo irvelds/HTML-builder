4cb826e20a1ad35caab16146fc4c4ee83154b904
irvelds <irvelds@mail.ru>
2023-05-06 22:32:51 +0300

    feat: add functionality task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const path = require('path');
+const fs = require('fs/promises');
+const folderCurrent = path.join(__dirname, 'files');
+const folderCopy = path.join(__dirname, 'files-copy');
 
+async function copyFolder() {
+  await fs.rm(folderCopy, { recursive: true, force: true });
+  await fs.mkdir(folderCopy, { recursive: true });
+  fs.readdir(folderCurrent, { withFileTypes: true })
+    .then(files => {
+      files.forEach(file => {
+        if (file.isFile()) {
+          fs.copyFile(path.join(folderCurrent, file.name), path.join(folderCopy, file.name));
+        }
+      });
+    });
+}
+copyFolder();
