513907ba4507c3b880f974862ca15951cb8938d5
irvelds <irvelds@mail.ru>
2023-05-06 23:37:55 +0300

    feat: finish task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,16 +3,20 @@ const fs = require('fs/promises');
 const folderCurrent = path.join(__dirname, 'files');
 const folderCopy = path.join(__dirname, 'files-copy');
 
-async function copyFolder() {
+async function copyFolder(folderCurrent, folderCopy) {
   await fs.rm(folderCopy, { recursive: true, force: true });
   await fs.mkdir(folderCopy, { recursive: true });
   fs.readdir(folderCurrent, { withFileTypes: true })
-    .then(files => {
-      files.forEach(file => {
-        if (file.isFile()) {
-          fs.copyFile(path.join(folderCurrent, file.name), path.join(folderCopy, file.name));
+    .then(items => {
+      items.forEach(item => {
+        if (item.isFile()) {
+          fs.copyFile(path.join(folderCurrent, item.name), path.join(folderCopy, item.name));
+        }
+        else if (item.isDirectory()) {
+          fs.mkdir(path.join(folderCurrent, item.name), { recursive: true });
+          copyFolder(path.join(folderCurrent, item.name), path.join(folderCopy, item.name));
         }
       });
     });
 }
-copyFolder();
+copyFolder(folderCurrent, folderCopy);
